54a7e76b8ec7745f68189b4d97ff0bc3512a05d1
Volodya Pabyrivkskyi <pabyrivskyi@gmail.com>
2022-11-21 22:09:12 +0200

    Add delay to secondary message write.

--- LogReplication/Secondary/Services/MessageService.cs
+++ LogReplication/Secondary/Services/MessageService.cs
@@ -7,14 +7,23 @@ namespace Secondary.Services
     public class MessageService : MessageServiceBase
     {
         private readonly MessageStore _messageStore;
+        private readonly ILogger<MessageService> _logger;
+        private readonly int _writeDelay;
 
-        public MessageService(MessageStore messageStore)
+        public MessageService(MessageStore messageStore, IConfiguration configuration, ILogger<MessageService> logger)
         {
             _messageStore = messageStore;
+            _logger = logger;
+            _writeDelay = configuration.GetSection("WriteDelay").Get<int>();
         }
 
         public override async Task<Response> InsertMessage(Message request, ServerCallContext context)
         {
+            if (_writeDelay > 0) 
+            {
+                _logger.LogInformation("Delaying message write to {delay} ms.", _writeDelay);
+                Thread.Sleep(_writeDelay);
+            }
             await _messageStore.InsertMessageAsync(request.Index, request.Value);
             return new Response();
         }
